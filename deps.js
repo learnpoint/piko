48120c29aaa3c9936fb28140dcd4b33dc8ee76f3
Karl West <mickel.west@gmail.com>
2021-04-09 17:24:56 +0200

    version bump

--- deps.js
+++ deps.js
@@ -1,5 +1,5 @@
-export * as path from "https://deno.land/std@0.91.0/path/mod.ts";
-export { bold, yellow, red, brightGreen } from "https://deno.land/std@0.91.0/fmt/colors.ts";
-export { Server, Status, STATUS_TEXT } from "https://deno.land/std@0.91.0/http/mod.ts";
-export { acceptWebSocket, acceptable } from "https://deno.land/std@0.91.0/ws/mod.ts";
+export * as path from "https://deno.land/std@0.92.0/path/mod.ts";
+export { bold, yellow, red, brightGreen } from "https://deno.land/std@0.92.0/fmt/colors.ts";
+export { Server, Status, STATUS_TEXT } from "https://deno.land/std@0.92.0/http/mod.ts";
+export { acceptWebSocket, acceptable } from "https://deno.land/std@0.92.0/ws/mod.ts";
 export * as marked from "https://cdn.jsdelivr.net/gh/markedjs/marked@2.0.1/lib/marked.esm.js";
--- version.js
+++ version.js
@@ -1,2 +1,2 @@
-export const version = "0.9.16";
-export const denoVersion = "1.8.2";
+export const version = "0.9.17";
+export const denoVersion = "1.8.3";

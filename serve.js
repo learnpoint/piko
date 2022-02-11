4b989f3970c6f736f2d27e33a51bb202ef3a822f
Karl West <mickel.west@gmail.com>
2022-02-11 19:31:49 +0100

    move mimeType into separate file

--- serve.js
+++ serve.js
@@ -1,6 +1,7 @@
 import { exists } from "./utils/exists.js";
 import { listenAndServe } from "./utils/listen_and_serve.js";
 import { Status, STATUS_TEXT } from "./utils/http_status.js";
+import { contentType } from "./utils/content_type.js";
 import { path } from "./deps.js";
 
 const RELOAD_DEBOUNCE = 100;
@@ -49,8 +50,8 @@ async function httpHandler(req) {
         return redirect(req, pathname(req) + "/" + searchString(req));
     }
 
-    // Valid mime type?
-    if (!mimeType.hasOwnProperty(path.extname(filePath))) {
+    // Valid file extension?
+    if (!contentType(filePath)) {
         return respond(req, Status.BadRequest, `Not Supported File Extension ${path.extname(filePath)}`);
     }
 
@@ -66,14 +67,14 @@ async function httpHandler(req) {
 
     try {
         // html?
-        if (mimeType[path.extname(filePath)].includes('text/html')) {
+        if (path.extname(filePath) === '.html') {
             const fileContent = await Deno.readTextFile(filePath);
             const body = fileContent.replace("</body>", `${browserReloadScript}</body>`);
-            headers.set("content-type", mimeType[path.extname(filePath)]);
+            headers.set("content-type", contentType(filePath));
             return respond(req, Status.OK, body, headers);
         } else {
             const file = await Deno.readFile(filePath);
-            headers.set("content-type", mimeType[path.extname(filePath)]);
+            headers.set("content-type", contentType(filePath));
             return respond(req, Status.OK, file, headers);
         }
     } catch (error) {
@@ -129,7 +130,7 @@ async function watchAndReload() {
 
 async function redirect(req, location) {
     const headers = new Headers();
-    headers.set("content-type", mimeType['.html']);
+    headers.set("content-type", 'text/html');
     headers.set("location", location);
 
     return respond(req, 302, null, headers);
@@ -137,7 +138,7 @@ async function redirect(req, location) {
 
 async function notFound(req) {
     const headers = new Headers();
-    headers.set("content-type", mimeType['.html']);
+    headers.set("content-type", 'text/html');
 
     let content;
 
@@ -195,52 +196,6 @@ function pathname(req) {
 function searchString(req) {
     return new URL(req.url).search;
 }
-
-const mimeType = {
-    '.avi': 'video/m-msvideo',
-    '.avif': 'image/avif',
-    '.bmp': 'image/bmp',
-    '.bz': 'application/x-bzip',
-    '.bz2': 'application/x-bzip2',
-    '.css': 'text/css; charset=utf-8',
-    '.csv': 'text/csv; charset=utf-8',
-    '.doc': 'application/msword',
-    '.docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    '.gz': 'application/gzip',
-    '.gif': 'image/gif',
-    '.htm': 'text/html; charset=utf-8',
-    '.html': 'text/html; charset=utf-8',
-    '.ico': 'image/x-icon',
-    '.jpeg': 'image/jpeg',
-    '.jpg': 'image/jpeg',
-    '.js': 'application/javascript; charset=utf-8',
-    '.json': 'application/json; charset=utf-8',
-    '.mjs': 'text/javascript; charset=utf-8',
-    '.mp3': 'audio/mpeg',
-    '.mp4': 'video/mp4',
-    '.mpeg': 'video/mpeg',
-    '.odp': 'application/vnd.oasis.opendocument.presentation',
-    '.ods': 'application/vnd.oasis.opendocument.spreadsheet',
-    '.odt': 'application/vnd.oasis.opendocument.text',
-    '.otf': 'font/otf',
-    '.png': 'image/png',
-    '.pdf': 'application/pdf',
-    '.ppt': 'application/vnd.ms-powerpoint',
-    '.pptx': 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-    '.rtf': 'application/rft',
-    '.svg': 'image/svg+xml',
-    '.txt': 'text/plain; charset=utf-8',
-    '.weba': 'audio/webm',
-    '.webm': 'video/webm',
-    '.webmanifest': 'application/manifest+json',
-    '.webp': 'image/webp',
-    '.woff': 'font/woff',
-    '.woff2': 'font/woff2',
-    '.xls': 'application/vnd.ms-excel',
-    '.xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    '.xml': 'application/xml',
-    '.zip': 'application/zip',
-};
 
 const browserReloadScript = `
 <script>

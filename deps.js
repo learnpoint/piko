d01e9e189a651d947fb7177e9e1644ac1137636e
Karl West <mickel.west@gmail.com>
2023-03-24 18:23:15 +0100

    use new url for yaml std library

--- deps.js
+++ deps.js
@@ -1,4 +1,4 @@
 export * as path from "https://deno.land/std@0.181.0/path/mod.ts";
 export { decode } from "https://deno.land/std@0.181.0/encoding/base64.ts";
-export { parse as yamlParse } from "https://deno.land/std@0.181.0/encoding/yaml.ts";
+export { parse as yamlParse } from "https://deno.land/std@0.181.0/yaml/parse.ts";
 export * as marked from "https://cdn.jsdelivr.net/gh/markedjs/marked@4.3.0/lib/marked.esm.js";

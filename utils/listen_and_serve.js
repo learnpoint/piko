6e6f88ac082c8b102d0490ece6696d84c678312c
Karl West <mickel.west@gmail.com>
2023-07-14 22:55:54 +0200

    upgrade to Deno.serve API

--- utils/listen_and_serve.js
+++ utils/listen_and_serve.js
@@ -1,45 +1,16 @@
 export async function listenAndServe(httpHandler, webSocketHandler, options = { port: null, muteLog: false }) {
 
-    const PORT_CANDIDATES = options.port ? [options.port] : [3333, 4444, 5555, 6666, 7777, 8888, 9999];
-    const listener = createListener(PORT_CANDIDATES);
-
-    if (!options.muteLog) {
-        console.log(`%c\nServer started at http://localhost:${listener.addr.port}/\n`, 'color:#4f4;');
+    if (options.port && isNaN(options.port)) {
+        console.log(`%c\nError. Invalid port: ${options.port}\n`, 'font-weight:bold;color:#f44;');
+        Deno.exit(1);
     }
 
-    for await (const connection of listener) {
-        connectionHandler(connection, httpHandler, webSocketHandler);
-    }
+    const PORT_CANDIDATES = options.port ? [Number(options.port)] : [3333, 4444, 5555, 6666, 7777, 8888, 9999];
+
+    recursiveTryListenAndServe(httpHandler, webSocketHandler, PORT_CANDIDATES, options.muteLog);
 }
 
-async function connectionHandler(connection, httpHandler, webSocketHandler) {
-    const httpConnection = Deno.serveHttp(connection);
-    try {
-        for await (const requestEvent of httpConnection) {
-            try {
-                await requestEvent.respondWith(requestHandler(requestEvent.request, httpHandler, webSocketHandler));
-            } catch {
-                // Typically: Connection Closed by Client
-            }
-        }
-    } catch {
-        // Typically: Connection Closed by Client
-    }
-}
-
-function requestHandler(request, httpHandler, webSocketHandler) {
-    if (request.headers.get("upgrade") === "websocket") {
-        return webSocketHandler(request);
-    } else {
-        return httpHandler(request);
-    }
-}
-
-function createListener(portCandidates) {
-
-    // Recursively try listen on port candidates.
-    // Return listener if successful.
-    // Exit if all candidates unsuccessfully tried.
+function recursiveTryListenAndServe(httpHandler, webSocketHandler, portCandidates, muteLog) {
 
     if (portCandidates.length < 1) {
         console.log('%c\nError. Port not available.\n', 'font-weight:bold;color:#f44;')
@@ -49,10 +20,27 @@ function createListener(portCandidates) {
     const port = portCandidates.shift();
 
     try {
-        return Deno.listen({ port });
-    } catch (err) {
-        if (err instanceof Deno.errors.AddrInUse) {
-            return createListener(portCandidates);
+        Deno.serve({
+            port,
+            handler: req => requestHandler(req, httpHandler, webSocketHandler),
+            onListen: () => muteLog ? {} : console.log(`%c\nServer started at http://localhost:${port}/\n`, 'color:#4f4;')
+        });
+
+    } catch (error) {
+
+        if (error instanceof Deno.errors.AddrInUse) {
+            return recursiveTryListenAndServe(httpHandler, webSocketHandler, portCandidates, muteLog);
+        } else {
+            console.log(error);
+            Deno.exit(1);
         }
+    }
+}
+
+function requestHandler(request, httpHandler, webSocketHandler) {
+    if (request.headers.get("upgrade") === "websocket") {
+        return webSocketHandler(request);
+    } else {
+        return httpHandler(request);
     }
 }

e63132ffb123c7cebaa6771c2b243751571f2f21
Karl West <mickel.west@gmail.com>
2023-11-24 20:39:04 +0100

    upgrade marked

--- deps.js
+++ deps.js
@@ -2,4 +2,4 @@ export * as path from "https://deno.land/std@0.204.0/path/mod.ts";
 export { decode } from "https://deno.land/std@0.204.0/encoding/base64.ts";
 export { parse as yamlParse } from "https://deno.land/std@0.204.0/yaml/parse.ts";
 export { ByteSliceStream } from "https://deno.land/std@0.204.0/streams/byte_slice_stream.ts";
-export * as marked from "https://cdn.jsdelivr.net/npm/marked@9.1.1/lib/marked.esm.js";
+export * as marked from "https://cdn.jsdelivr.net/npm/marked@10.0.0/lib/marked.esm.js";

20540221e82311ba18f789e2de885da417ea39f5
Karl West <mickel.west@gmail.com>
2025-01-17 08:58:00 +0100

    upgrade deno std modules

--- deps.js
+++ deps.js
@@ -1,4 +1,4 @@
-export { decodeBase64 as decode } from "jsr:@std/encoding@1.0.5/base64";
+export { decodeBase64 as decode } from "jsr:@std/encoding@1.0.6/base64";
 export * as path from "jsr:@std/path@1.0.8";
 export { ByteSliceStream } from "jsr:@std/streams@1.0.8/byte-slice-stream";
 export { parse as yamlParse } from "jsr:@std/yaml@1.0.5/parse";

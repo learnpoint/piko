3f726add477b2bb88cabafbf5172690e28d93a05
Karl West <mickel.west@gmail.com>
2025-02-14 16:30:59 +0100

    upgrade markedjs

--- deps.js
+++ deps.js
@@ -2,4 +2,4 @@ export { decodeBase64 as decode } from "jsr:@std/encoding@1.0.7/base64";
 export * as path from "jsr:@std/path@1.0.8";
 export { ByteSliceStream } from "jsr:@std/streams@1.0.9/byte-slice-stream";
 export { parse as yamlParse } from "jsr:@std/yaml@1.0.5/parse";
-export * as marked from "https://cdn.jsdelivr.net/npm/marked@15.0.6/lib/marked.esm.js";
+export * as marked from "https://cdn.jsdelivr.net/npm/marked@15.0.7/lib/marked.esm.js";
